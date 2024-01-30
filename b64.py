da892f1d711f479f10c005dc0a8a3618d82a74cf
john9405 <835292875@qq.com>
2024-01-30 16:30:57 +0800

    Add files via upload

---
+++ b64.py
@@ -0,0 +1,39 @@
+""" base64 加解密工具 """
+import base64
+import tkinter as tk
+from tkinter import messagebox, ttk
+
+
+class Base64GUI:
+    def __init__(self, master) -> None:
+        self.master = master
+
+        input_frame = ttk.LabelFrame(master, text="Input")
+        input_frame.pack(side="left")
+        self.input_box = tk.Text(input_frame)
+        self.input_box.pack()
+        bframe = tk.Frame(master)
+        bframe.pack(side="left")
+        ebtn = ttk.Button(bframe, text="加密", command=self.encrypto)
+        ebtn.pack(padx=5, pady=5)
+        dbtn = ttk.Button(bframe, text="解密", command=self.decrypto)
+        dbtn.pack(padx=5, pady=5)
+        output_frame = ttk.LabelFrame(master, text="Output")
+        output_frame.pack(side="left")
+        self.output_box = tk.Text(output_frame)
+        self.output_box.pack()
+
+    def encrypto(self):
+        input_text = self.input_box.get(1.0, tk.END)
+        res = base64.b64encode(input_text.encode("utf-8")).decode("utf-8")
+        self.output_box.delete(1.0, tk.END)
+        self.output_box.insert(1.0, res)
+
+    def decrypto(self):
+        try:
+            input_text = self.input_box.get(1.0, tk.END)
+            res = base64.b64decode(input_text.encode("utf-8")).decode("utf-8")
+            self.output_box.delete(1.0, tk.END)
+            self.output_box.insert(1.0, res)
+        except Exception as e:
+            messagebox.showerror("错误", e)
