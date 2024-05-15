c55ab23d580d37a79aa4a801395217d2ea4dca19
Zhihong Yang <835292875@qq.com>
2024-05-15 18:01:38 +0800

    修改静态文件位置

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,4 @@ import os
 
 USER_DIR = os.path.expanduser("~")
 WORK_DIR = os.path.join(USER_DIR, "apitest")
-BASE_DIR = os.path.dirname(__file__)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
