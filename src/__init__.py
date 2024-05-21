76600e71bd0b639df762fc758440d659d345907b
Zhihong Yang <835292875@qq.com>
2024-05-21 11:07:32 +0800

    修改缓存文件地址

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 import os
 
 USER_DIR = os.path.expanduser("~")
-WORK_DIR = os.path.join(USER_DIR, "apitest")
+WORK_DIR = os.path.join(USER_DIR, "Postman")
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
