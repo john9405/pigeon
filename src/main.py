2a8bdd32347a1ffe887df9b05727f50b8c4bdae9
yzhh <835292875@qq.com>
2024-10-12 16:54:28 +0800

    适配macOS的 couldn't recognize image data问题

--- src/main.py
+++ src/main.py
@@ -32,45 +32,15 @@ class MainWindow:
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.root.geometry("800x600")
         self.root.after(300000, self.write_to_disk)
-        self.images = [
-            tk.PhotoImage(
-                name='col',
-                data='iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAchJREFUWEftV21ygjAQfSsHKT1J5b/O9ARFTtJ6EtATdEb/iycp3qNm6+4QDJgW0kFnOpWfZLN5+/YjL4Se7znnuM9m6Pp7RlXXlr7bPFvx62nxbajzALuKDbJtRqXs8QKYr3kHxjTAabApGyQC4gLAfM0LMHLHYwXCBXXBJ8qGdlDVJqXHFgDJ9zFC3hgSSmYoVb/5iHDgIypLd9d/ZHAGcFXaCWV0RCZFOC84PzG6UEIMEmXgqodb6gjl5oWSCwBKywQf1o5HrHxixDbaJmJC2mKgVXSMImIsPydIiYZ3gdTJNqWlDUJ9Gjw1vuqgJDgXlKbABdA1CCk821aa0hWfXGmTSwGvbFcNAgCDPU2wCzkcgLZUw0BdaBphhDgIgFAZOn59I7aVjnqu9DIQGHWw+Z8AMM7IbXPT3Ka9DIiB207BHHs2/NRl3ja8A7gzcGfgXzGgkqw7KK7JgF7PjjC9PQB3UtYS7bYMnAFUkUEi1zjNcp42AoRRbBaUjXEH+PSACBcGCjfN1BGlaiB6fgwQzHhwn3eubDuLZdFwjlYf42Cvjzrn3TV9FwgLqoRHlOSdg5qcewHYn1oPEWKhbjQmDPb2aebz+QWOe7dO2oOpygAAAABJRU5ErkJggg=='
-            ),
-            tk.PhotoImage(
-                name='var',
-                data='iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAcRJREFUWEftV21ygkAMfYHeo3qS6n+d6QlETqKeBPAEnaH/pScpvUchZYO7Liv1o8BMHeWXLG7yNpu8vBCc5zXi0Tcwctf7en8C8reQcm2P9I9ZxBPyEGFA5+YQhMwvECogAkCduvDw2dcpL7STpwGNBcA85giE5YUbe/sbA+saQMLq9PW9M+J0SWFvXixDEmkfERgTWSZkGkAFpn64xPQ9pGwIAMrmLOFV5XR9WwAkRzyMdOa60ZEK8rECkKQLik9F7+oINDa05IdbQX6JsV3nR2CvvYIGAEBKxza654+dXrsPAPMtK84IVE4o2iYPjQjIGiGAhw83J7rlAJD7JaaGNQkZF9i4AIoakPCKeyWdAYCwAUvPMLzRAsDQussrDwCPCNx+BLhEaGV97rxL2dllmAZkVNefumEbFQsRlXhhRiJt/RciUt/d1t75Cu6vF0i4NfMRsnRBU7sb7tuxpt6jbtm5HRvRqnidsWnr9XZOnJNzV+dA39rw/wMAITyn67pEpTGDMOJalm95Z7Q6kFdDipndujg72suiEQ5zpwbg6rpenZ4wpljzMJzaanV4BELhqmIaPL0fnSbMeB4KAxG+7Bz7AR7bNVkLvYTOAAAAAElFTkSuQmCC'
-            ),
-            tk.PhotoImage(
-                name='his',
-                data='iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAn9JREFUWEetV9tx4zAMXFjXR5xO7P94JhXEViU5VyIpFdyM8h+nklMhsXAGJdESCT6UnH4FAgtgQS4IP/ieK97K8T8ldd91Q7kHJdjXBkci7MDYKec6EARIw1d07yVdcnwnAUjgK+H15vyU49DaMOqCcU5VJwrgqeHXm8HvVYGXxh0D9fuRziEfKgCTdYEqUGrxNfR8KDnAEC4YPgS+ruix16rhATDBN/jQHLJUo8en1l/DEWBLhGOgXSoID8DhjT/czCVwrIxu1pawfvu69kiPc/sFADV4j32I0cIRcRYCp3KIUbcnKicQFsBTxTsaSn8nciK4JajjdO7j8MYnMKqQXwvAy55Qti9Uh1i1sCdc2hfah2yVSthWGAAeyoTD8cydKwl7jdg8VncAUHM1Z+70Mzb/ayqgJjm2bQDQ8N/Z2HlM1YCsBTBWQeJMn4kzAbhN2vhFCOWQK7sF0zmXZ0WPR/LYnyCf6iyDM3LOJaO0mlwCCqrUA6KRsLjCzrb813xoyf4fADpb5Xk+z0dZmbbSa0HOBHgVCI/LgtAqAJedKwB4N5yHwyG0ywFDQm88MqfAkEqu70J/hjVVpE6BUs6seyB2SQWv74a9cVev4tw2rAGh9V8IagCEbqk1AVK2h3n2AKZxvz/Hjv5bK0Ky3w2j4O4CxwJQqrAwTGUY7Lvz0IldK+J+/FxF5IuHlXJs8V4owV1+eZpQlVGEC19xzl02xitXVNBCKWttVWV5cB9g1NjgU1NKMyEqC4wn0UOcCi4mmpZz+jzfB2M7AWJjHd2MIvI6j5MZ61lyN7T3RP5+aNaxXz2anGc9C8CUrt1+CmyZ8UDDSgaeVrTA1hQr1z/YEshuS7ifxQAAAABJRU5ErkJggg=='
-            ),
-            tk.PhotoImage(
-                name='right',
-                data='iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAM5JREFUOE+1ksENgkAUROejBRBsABMhsQqlEzguFiEWYTgClUAXJuKBBkQKwP2KiagcXJSwt012Zt5MljDw0EA9xjfQw4NJPI2Yaq8Sy6JLrCSY7Y8uE0UAF5KuTtdEaWCEpwjM7jOZmL3zxo7be4Oo8SQFyOw9KCGQqJOGhvQwX2uMtLe4jaa4FAvvfwNwUfr2nF4V3hm+12Egu/iW0yh+HvGuCEph7domqu5GmG/BCJp3kuBUwso+WFUGj3+gaSsJTrriXhVUAcoNRje4AS5HThGKogbHAAAAAElFTkSuQmCC'
-            ),
-            tk.PhotoImage(
-                name='left',
-                data='iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAMhJREFUOE+1k80RgjAQhfehBSAVwAFm7AI6kSNYBNqEOWIn0IUHPNCASAHIOhmGH3VGiBlzTPZ7eZu3AWkuaPL0HwFTFD6YEkYT1tG2/Obyw4GEDaZMQi0oqCM3XyywORUZiPweAHN423vnWQFTXGzwOp3CI8STFmD3+0yU32M3gIQNXmVE4+HSZFo0jr6AvO03F1xWsee8pGCJa0rMu6EF0KGK3OPsI04LLFEknQhs5Rh7oW4WOG3xCJQHaWkCw6yoAu/12p/pCc44SU1DCcWEAAAAAElFTkSuQmCC'
-            ),
-            tk.PhotoImage(
-                name='add',
-                data='iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAASJJREFUOE+lk11ugkAUhc+lLsDqBjQpJt0FrER9hC5CXYTwqDuBXZiAibOBUt8r3GZmFJlBSG3ncebeb865P4R/HrLzh/Fh4mAwZ4ZHgAewYJAgIlHhe3MO3kUzxwAM49xzGEm3KBYg2heBu7nF1IBHyRXBBy7C4cHJ+JV5+fkx28u7GjCKshNAk2ZgEbrqfRTlbKpiUVHpSzs6ID7uwLywpXcD1NdraeXPAAbSr9D1rxK1fOn5HLhpX2dVl1RNWBThbGp4pEZxuiB3ACAtKsBrlCe65+bpq4FpIc5XYKyfAeh5eFsqBVrWS/L7NgIVXaZ1GyVkvM0WTLSzB6Z0ytQepGaxjVHWEKxsJXdozyjfgtrLJF/aia1deNQ2CbO3z477AXnBoxFYbQ0+AAAAAElFTkSuQmCC1'
-            ),
-            tk.PhotoImage(
-                name='subtract',
-                data='iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAPxJREFUOE+dk0EOgjAQRf8Q3KsnwERMvAWcRF3CJdRLyFI9idzCREzkBOhe7RhaFEq0VbvszLz2z58hGE432XuXaJybcqgdLIscdOZgntYxzkFOKnBdtoEaoJ9kczAWn18sQbQpIn/5zHkB7MUNLGHxhEhAN8kCh7EzadVjnAu6h6UcCegnx7Wu2Y5iID3HfqgAq8MJIM9e1szgvIhHgwqQsQw1tL2DKYfciWp0BZCX7J6+B9QWF7FPf0vQetBbZTsCgp96IOdhOKtsbMj4kiLoNnjZqKy0TWFNFoTwEvmp6nt19A4bvtFyyrBMIqhnQ+2AwG1rXKZ3m2lb5wfrMX4RRvB8VgAAAABJRU5ErkJggg=='
-            )
-        ]
         col_top = None
         history_top = None
         state_bar = ttk.Frame(self.root)
         state_bar.pack(side=tk.BOTTOM, fill=tk.X)
         ttk.Separator(self.root, orient=tk.HORIZONTAL).pack(side=tk.BOTTOM, fill=tk.X)
         side_bar = ttk.Frame(self.root)
-        ttk.Button(side_bar,image='col',command=lambda :self.main_bar_show('col', col_top, history_top)).pack()
-        ttk.Button(side_bar,image='var',command=lambda :self.main_bar_show('var', col_top, history_top)).pack()
-        ttk.Button(side_bar,image='his',command=lambda :self.main_bar_show('his', col_top, history_top)).pack()
+        ttk.Button(side_bar, text='C', width=1, command=lambda: self.main_bar_show('col', col_top, history_top)).pack()
+        ttk.Button(side_bar, text='V', width=1, command=lambda: self.main_bar_show('var', col_top, history_top)).pack()
+        ttk.Button(side_bar, text='H', width=1, command=lambda: self.main_bar_show('his', col_top, history_top)).pack()
         side_bar.pack(side=tk.LEFT, fill=tk.Y)
         panel_window = ttk.PanedWindow(self.root, orient="horizontal")
         main_bar = ttk.Frame(panel_window)
@@ -104,8 +74,10 @@ class MainWindow:
         tool_menu.add_command(label="MD5", command=lambda: self.new_tab(MD5GUI, "MD5"))
         tool_menu.add_command(label="Password", command=lambda: self.new_tab(GenPwdWindow, "Password"))
         tool_menu.add_command(label="Timestamp", command=lambda: self.new_tab(TimestampWindow, "Timestamp"))
-        tool_menu.add_command(label="Regular Expression ", command=lambda: self.new_tab(RegexWindow, "Regular Expression "))
-        tool_menu.add_command(label="Common Regular Expressions", command=lambda: self.new_tab(CommonlyUsed, "Common Regular Expressions"))
+        tool_menu.add_command(label="Regular Expression ",
+                              command=lambda: self.new_tab(RegexWindow, "Regular Expression "))
+        tool_menu.add_command(label="Common Regular Expressions",
+                              command=lambda: self.new_tab(CommonlyUsed, "Common Regular Expressions"))
         tool_menu.add_command(label="RSA Key", command=lambda: self.new_tab(RSAKeyFrame, "RSA Key"))
         tool_menu.add_command(label="RSA Public Key", command=lambda: self.new_tab(RsaPublicKey, "RSA Public Key"))
         tool_menu.add_command(label="RSA Check", command=lambda: self.new_tab(RSACheck, "RSA Check"))
@@ -120,10 +92,10 @@ class MainWindow:
         self.root.config(menu=menu)
 
         ttk.Sizegrip(state_bar).pack(side="right", anchor="se")
-        ttk.Button(state_bar, image="subtract", command=self.close_tab).pack(side="right", padx=(0,3))
-        ttk.Button(state_bar, image="add", command=self.new_request).pack(side="right")
-        ttk.Button(state_bar, image="right", command=self.next_tab).pack(side="right")
-        ttk.Button(state_bar, image="left", command=self.previous_tab).pack(side="right")
+        ttk.Button(state_bar, text="-", command=self.close_tab, width=1).pack(side="right", padx=(0, 3))
+        ttk.Button(state_bar, text="+", command=self.new_request, width=1).pack(side="right")
+        ttk.Button(state_bar, text=">", command=self.next_tab, width=1).pack(side="right")
+        ttk.Button(state_bar, text="<", command=self.previous_tab, width=1).pack(side="right")
 
     def main_bar_show(self, name, col_box, his_box):
         if self.sidebar != name:
@@ -196,7 +168,7 @@ class MainWindow:
         name = kwargs.get('name')
         if name is not None:
             self.nbb.tab(self.nbb.index("current"), text=name)
-    
+
     def collection(self, **kwargs):
         if f"col_{kwargs['item_id']}" in self.tag_list:
             self.nbb.select(self.tag_list.index(f"col_{kwargs['item_id']}"))
@@ -224,7 +196,7 @@ class MainWindow:
             self.nbb.add(frame, text=kwargs["data"]['name'])
             self.nbb.select(frame)
         else:
-            self.new_request(kwargs["data"], item_id=kwargs["item_id"], path=kwargs['path'],)
+            self.new_request(kwargs["data"], item_id=kwargs["item_id"], path=kwargs['path'], )
         self.tag_list.append(f"col_{kwargs['item_id']}")
 
     def history(self, **kwargs):
